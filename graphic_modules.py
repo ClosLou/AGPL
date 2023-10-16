0019e8d1804fb6371f55eed29fa2e83301facd30
louclos <louclos@gmail.com>
2023-10-16 18:20:53 +0200

    Add docstring

--- graphic_modules.py
+++ graphic_modules.py
@@ -3,18 +3,28 @@ from tree_gen import Node, AtomType, Classe
 from dsplot import tree
 
 def tree_print(tree : Node, sep : str = ' '):
-    '''
-    Fonction qui permet d'afficher un arbre dans la console
-    '''
+    """Fonction qui permet d'afficher un arbre dans la console
+
+
+    Args:
+        tree (Node): Arbre à afficher
+        sep (str, optional): Séparateur entre les noeuds. Par défaut ' '.
+    """
     tree_print_aux(tree, sep)
     print()
 
 def tree_print_aux(tree : Node, sep : str = ' '):
-    '''
-    Fonction auxiliaire qui permet d'afficher un arbre dans la console
+    """Fonction auxiliaire qui permet d'afficher un arbre dans la console
     il s'agit d'un parcours préfixe de l'arbre avec des test afin de savoir si on doit 
     afficher un crochet pour les neouds de type star ou une parenthèse et une bare 
     oblique pour les noeuds de type un 
+
+    Args:
+        tree (Node): Arbre à afficher
+        sep (str, optional): Séparateur entre les noeuds. Par défaut ' '.
+    """
+    '''
+
     
     '''
     if tree.classe==Classe.Star:
@@ -41,11 +51,22 @@ def tree_print_aux(tree : Node, sep : str = ' '):
         tree_print_aux(tree.right, sep)
     return
 
-def draw_tree(tree):
+def draw_tree(tree : Node):
+    """Fonction qui permet de dessiner un arbre dans la console
+
+    Args:
+        tree (Node): Arbre à dessiner
+    """
     draw_tree_aux(tree,0)
     print() # affichage d'une fin de ligne
     
-def draw_tree_aux(tree, level):
+def draw_tree_aux(tree : Node, level : int):
+    """Fonction auxiliaire qui permet de dessiner un arbre dans la console
+
+    Args:
+        tree (Node): Arbre à dessiner
+        level (int): Niveau de l'arbre
+    """
     if tree.classe==Classe.Atom:
         if tree.AType == AtomType.Terminal:
             print('---'*level,"'",tree.value,"'", sep='')
@@ -60,22 +81,32 @@ def draw_tree_aux(tree, level):
     return
 
  
-def read_tree_to_list_for_dsplot(tree):
-    '''
-    Fonction qui permet de lire un arbre et de le retourner sous forme de liste 
+def read_tree_to_list_for_dsplot(tree : Node) -> list:
+    """Fonction qui permet de lire un arbre et de le retourner sous forme de liste 
     dans le format de dsplot.tree.BinaryTree
-    '''
+
+    Args:
+        tree (Node): Arbre à lire
+        
+    Returns:
+        list: Liste des valeurs de l'arbre
+    """
     list_value = []
     read_tree_to_list_for_dsplot_aux(tree, list_value, 0)
     if len(list_value[-1]) == list_value[-1].count(None):
         list_value.pop()
     return sum(list_value,[])
 
-def read_tree_to_list_for_dsplot_aux(tree, list_value, index):
-    '''
-    Fonction auxiliaire qui permet de lire un arbre et de le retourner sous forme de liste 
+def read_tree_to_list_for_dsplot_aux(tree : Node, list_value : list, index : int):
+    """Fonction auxiliaire qui permet de lire un arbre et de le retourner sous forme de liste 
     les valeurs sont stockées en fonction de leur niveau dans l'arbre
-    '''
+
+    Args:
+        tree (Node): Arbre à lire
+        list_value (list): Liste des valeurs de l'arbre
+        index (int): Niveau de l'arbre
+    """
+
     if tree == None:
         if len(list_value) <= index:
             list_value.append([None])
@@ -102,16 +133,14 @@ def read_tree_to_list_for_dsplot_aux(tree, list_value, index):
         read_tree_to_list_for_dsplot_aux(tree.left, list_value, index+1)
         read_tree_to_list_for_dsplot_aux(tree.right, list_value, index+1)
 
-def create_tree_dsplot(node_tree, output_path):
-    '''
-    Fonction qui permet de créer un plot de l'arbre tree avec dsplot.tree.BinaryTree
+def create_tree_dsplot(node_tree : Node, output_path : str):
+    """Fonction qui permet de créer un plot de l'arbre tree avec dsplot.tree.BinaryTree
     et de le sauvegarder dans un fichier png à l'adresse output_path
-    '''
+
+    Args:
+        node_tree (Node): Arbre à dessiner
+        output_path (str): Chemin du fichier de sortie
+    """
     tree_dsplot = tree.BinaryTree(read_tree_to_list_for_dsplot(node_tree))
     tree_dsplot.plot(output_path=output_path)
     return
-
-
-
-
-
--- tree_gen.py
+++ tree_gen.py
@@ -1,11 +1,22 @@
 from enum import Enum
 
 class AtomType(Enum):
+    """Classe qui permet de représenter le type de noeud atom
+    soit un terminal, soit un non terminal
+
+    Args:
+        Enum (class): Classe de base d'énumération
+    """
     Terminal = 1
     NonTerminal = 2
     
 
 class Classe(Enum):
+    """Classe qui permet de représenter le type d'un noeud d'un arbre
+
+    Args:
+        Enum (class): Classe de base d'énumération
+    """
     Conc = 1
     Union = 2
     Star = 3
@@ -14,9 +25,8 @@ class Classe(Enum):
     
 
 class Node:
-    '''
-    Classe qui permet de représenter un noeud d'un arbre
-    '''
+    """Classe qui permet de représenter un noeud d'un arbre
+    """
     def __init__(self, left = None, right = None, classe = None, action = None, value = None, AType = None):
         self.left = left
         self.right = right
@@ -27,24 +37,65 @@ class Node:
         
 
 def gen_conc(left : Node, right : Node) -> Node:
-    '''
-    Fonction qui permet de générer un noeud de type concaténation
-    '''
+    """Fonction qui permet de générer un noeud de type concaténation
+
+    Args:
+        left (Node): Noeud gauche
+        right (Node): Noeud droit
+
+    Returns:
+        Node: Noeud concaténation
+    """
     new_node = Node(left, right, Classe.Conc,  value= '.')
     return new_node
 
 def gen_union(left : Node, right : Node) -> Node:
+    """Fonction qui permet de générer un noeud de type union
+    
+    Args:
+        left (Node): Noeud gauche
+        right (Node): Noeud droit
+
+    Returns:
+        Node: Noeud union
+    """
     new_node = Node(left, right, Classe.Union,  value= '+')
     return new_node
 
 def gen_star(left : Node) -> Node:
+    """Fonction qui permet de générer un noeud de type star
+
+    Args:
+        left (Node): Noeud gauche
+
+    Returns:
+        Node: Noeud star
+    """
     new_node = Node(left, classe = Classe.Star,  value= '*')
     return new_node
 
 def gen_un(left : Node) -> Node:
+    """Fonction qui permet de générer un noeud de type un
+
+    Args:
+        left (Node): Noeud gauche
+
+    Returns:
+        Node: Noeud un
+    """
     new_node = Node(left, classe = Classe.Un, value= '(\\\\)')
     return new_node
 
 def gen_atom(value : str, action : int, AtomType : AtomType) -> Node:
+    """Fonction qui permet de générer un noeud de type atom
+
+    Args:
+        value (str): Valeur du noeud
+        action (int): Action du noeud
+        AtomType (AtomType): Type du noeud
+
+    Returns:
+        Node: Noeud atom
+    """
     new_node = Node(classe = Classe.Atom, action = action, value = value, AType = AtomType)
     return new_node 
