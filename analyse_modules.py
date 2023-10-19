ed8923ac1374a444006afa8acaefdb7a598741f3
Louis Closson <E221478M@etu.univ-nantes.fr>
2023-10-19 15:04:08 +0200

    small changes

--- analyse_modules.py
+++ analyse_modules.py
@@ -47,57 +47,60 @@ def analyse(node : Node, grammar : str)-> bool :
 
 def analyse_aux(node : Node, grammar : str)-> bool :
     global index
-    print(node.value, end = ' ')
-    if node.classe == Classe.Conc :
-        if analyse_aux(node.left, grammar) :
-            return analyse_aux(node.right, grammar)
-        else:
-            return False
-        
-    elif node.classe == Classe.Union :
-        if analyse_aux(node.left, grammar) :
-            return True
-        else:
-            return analyse_aux(node.right, grammar)
-        
-    elif node.classe == Classe.Star :
-        while analyse_aux(node.left, grammar) :
-            pass
-        return True
-    
-    elif node.classe == Classe.Un :
-        if analyse_aux(node.left, grammar) :
-            pass
-        return True
-    
-    elif node.classe == Classe.Atom :
-        if node.AType == AtomType.Terminal :
-            if node.value == '->'  :
-                return isArrow(grammar)
-            elif node.value == 'IDnter' :
-                return isIDnter(grammar)
-            elif node.value == 'Elter' :
-                return isElter(grammar)
-            elif node.value == grammar[index] :
-                index += 1
-                return True
-            else :
-                return False
-            
-        else : #NonTerminal
-            if node.value == 'S':
-                next_node = A[0]
-            elif node.value == 'N':
-                next_node = A[1]
-            elif node.value == 'E':
-                next_node = A[2]
-            elif node.value == 'T':
-                next_node = A[3]
-            elif node.value == 'F':
-                next_node = A[4]
+    if index == len(grammar) :
+        return False
+    match node.classe :
+        case Classe.Conc :
+            if analyse_aux(node.left, grammar) :
+                return analyse_aux(node.right, grammar)
             else:
                 return False
-            return analyse_aux(next_node, grammar)
+            
+        case Classe.Union :
+            if analyse_aux(node.left, grammar) :
+                return True
+            else:
+                return analyse_aux(node.right, grammar)
+            
+        case Classe.Star :
+            while analyse_aux(node.left, grammar) :
+                pass
+            return True
+        
+        case Classe.Un :
+            if analyse_aux(node.left, grammar) :
+                pass
+            return True
+        
+        case Classe.Atom :
+            if node.AType == AtomType.Terminal :
+                if node.value == '->'  :
+                    return isArrow(grammar)
+                elif node.value == 'IDnter' :
+                    return isIDnter(grammar)
+                elif node.value == 'Elter' :
+                    return isElter(grammar)
+                elif node.value == grammar[index] :
+                    index += 1
+                    return True
+                else :
+                    return False
+                
+            else : #NonTerminal
+                match node.value :
+                    case 'S':
+                        next_node = A[0]
+                    case 'N':
+                        next_node = A[1]
+                    case 'E':
+                        next_node = A[2]
+                    case 'T':
+                        next_node = A[3]
+                    case 'F':
+                        next_node = A[4]
+                    case other :
+                        return False
+                return analyse_aux(next_node, grammar)
     
                     
                 
--- main.py
+++ main.py
@@ -6,16 +6,18 @@ from analyse_modules import analyse
 
 if __name__ == '__main__':
     grammars = [
-        "S->['a'].'b',S->[N.'->'.E.','].';',N->'IDnter',E->T.['+'.T],T->F.['.'.F],F->'IDnter'+'Elter'+'('.E.')'+'['.E.']'+'(/'.E.'/)',;"
+        "S->[N.'->'.E.','].';',;",
+        "N->'IDnter',;",
+        "E->T.['+'.T],;",
+        "T->F.['.'.F],;",
+        "F->'IDnter'+'Elter'+'('.E.')'+'['.E.']'+'(/'.E.'/)',;"
         ]
     for grammar in grammars:
         if analyse(A[0], grammar) :
-            print("La grammaire est correcte")
+            print(f'La grammaire : "{grammar}" est correcte', end = '\n\n')
         else:
-
-            print("La grammaire n'est pas correcte")
-        print(grammar, end = '\n\n')
-
+            print(f'La grammaire "{grammar}" n\'est pas correcte', end = '\n\n')
+    
     # create_tree_dsplot(A1, 'A1.png')
     # create_tree_dsplot(A2, 'A2.png')
     # create_tree_dsplot(A3, 'A3.png')
