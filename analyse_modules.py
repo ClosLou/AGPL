b78b1eab6d619a6cb296ca394bd0e6cefc0450ed
Louis Closson <E221478M@etu.univ-nantes.fr>
2023-10-23 18:03:42 +0200

    pgtirengmqezwijgnikernqgùoqewokgjdf!wlbj,kdsfwnfg:eqgokjbnlsewigjmqoeitjgmqeigkjnmsoewikjgmqekjgbùqe)ihbjqeù)ikjgqù)eijgùiqet

--- analyse_modules.py
+++ analyse_modules.py
@@ -1,132 +1,120 @@
 from constantes import A
 from tree_gen import *
 
-
-def isArrow(grammar : str) -> bool:
-    """Fonction qui permet de savoir si la grammaire contient une flèche
-
-    Args:
-        grammar (str): grammaire à analyser
-
-    Returns:
-        bool: True si la grammaire contient une flèche, False sinon
-    """
-    global index
-    if grammar[index:index+2] == '->':
-        index += 2
-        return True
-    else:
-        return False
-    
-def isElter(grammar : str) -> bool:
-    """Fonction qui permet de savoir si la grammaire contient un élément terminal
-
-    Args:
-        grammar (str): grammaire à analyser
-
-    Returns:
-        bool: True si la grammaire contient un élément terminal, False sinon
-    """
-    global index
-    if grammar[index] == "'":
-        if index >= len(grammar) :
-            return False
-        else :
+def scan(grammar)-> list:
+    grammar_list = []
+    while grammar:
+        if grammar[0:2] == '->':
+            grammar_list.append(('->',AtomType.Terminal))
+            grammar = grammar[2:]
+        elif grammar[0:2] == '(/':
+            grammar_list.append(('(/',AtomType.Terminal))
+            grammar = grammar[2:]
+        elif grammar[0:2] == '/)':
+            grammar_list.append(('/)',AtomType.Terminal))
+            grammar = grammar[2:]
+        elif grammar[0] == "'":
+            if len(grammar) <= 1 :
+                print('Erreur lors du scan de la grammaire : guillemet fermant manquant')
+                return [] #TODO : gérer les erreurs
+            index = 1
+            while grammar[index] != "'" and not grammar[index-1] == '\\' : 
+                # On ne prend pas en compte les ' qui sont précédés d'un \
+                # car ils ne sont pas des délimiteurs
+                if  len(grammar) <= index :
+                    print('Erreur lors du scan de la grammaire : guillemet fermant manquant')
+                    return [] #TODO : gérer les erreurs
+                index += 1
             index += 1
-        while grammar[index] != "'" and not grammar[index-1] == '\\' : 
-            # On ne prend pas en compte les ' qui sont précédés d'un \
-            # car ils ne sont pas des délimiteurs
-            if index >= len(grammar) :
-                return False
-            index += 1
-        index += 1
-        return True
-    else:
-        return False
-    
-def isIDnter(grammar : str) -> bool:
-    """Fonction qui permet de savoir si la grammaire contient un élément non terminal
-
-    Args:
-        grammar (str): grammaire à analyser
-
-    Returns:
-        bool: True si la grammaire contient un élément non terminal, False sinon
-    """
-    global index
-    if grammar[index] in 'SNETF' :
-        index += 1
-        return True
-    else :
-        return False
-
-
-def analyse(node : Node, grammar : str)-> bool :
+            grammar_list.append((grammar[0:index], AtomType.Terminal))
+            grammar = grammar[index:]
+        elif grammar[0] == ',' or grammar[0] == ';' or grammar[0] == '+' or grammar[0] == '.' or grammar[0] == '[' or grammar[0] == ']' or grammar[0] == '(' or grammar[0] == ')':
+            grammar_list.append((grammar[0], None))
+            grammar = grammar[1:]
+        else:
+            grammar_list.append((grammar[0], AtomType.NonTerminal))
+            grammar = grammar[1:]
+    return grammar_list
+        
+        
+def analyse(node : Node, grammar_list : str)-> bool :
     """Fonction qui permet d'analyser une grammaire
 
     Args:
         node (Node): noeud de l'arbre
-        grammar (str): grammaire à analyser
+        grammar_list (str): grammaire à analyser
 
     Returns:
         bool: True si la grammaire est correcte, False sinon
     """
     global index
     index = 0
-    if analyse_aux(node, grammar) and index == len(grammar):
+    if analyse_aux(node, grammar_list) and index == len(grammar_list):
         return True
     else:
         return False
 
-def analyse_aux(node : Node, grammar : str)-> bool :
+
+
+
+def analyse_aux(node : Node, grammar_list : list)-> bool :
     """Fonction auxiliaire qui permet d'analyser une grammaire
 
     Args:
         node (Node): noeud de l'arbre
-        grammar (str): grammaire à analyser
+        grammar_list (str): grammaire à analyser
 
     Returns:
         bool: True si la grammaire est correcte, False sinon
     """
     global index
-    if index == len(grammar) :
+    if index >= len(grammar_list) :
         return False
     match node.classe :
         case Classe.Conc :
-            if analyse_aux(node.left, grammar) :
-                return analyse_aux(node.right, grammar)
+            if analyse_aux(node.left, grammar_list) :
+                return analyse_aux(node.right, grammar_list)
             else:
                 return False
             
         case Classe.Union :
-            if analyse_aux(node.left, grammar) :
+            index_tmp = index # On sauvegarde l'index de la dernière réussite
+            if analyse_aux(node.left, grammar_list) :
                 return True
             else:
-                return analyse_aux(node.right, grammar)
+                index = index_tmp # On restaure l'index de la dernière réussite
+                return analyse_aux(node.right, grammar_list)
             
         case Classe.Star :
-            while analyse_aux(node.left, grammar) :
-                pass
+            index_tmp = index # On sauvegarde l'index de la dernière réussite
+            while analyse_aux(node.left, grammar_list) :
+                index_tmp = index # On sauvegarde l'index de la dernière réussite
+            index = index_tmp # On restaure l'index de la dernière réussite
             return True
         
         case Classe.Un :
-            if analyse_aux(node.left, grammar) :
-                pass
+            index_tmp = index
+            if analyse_aux(node.left, grammar_list) :
+                index_tmp = index # On sauvegarde l'index de la dernière réussite
+            index = index_tmp # On restaure l'index de la dernière réussite
             return True
         
         case Classe.Atom :
             if node.AType == AtomType.Terminal :
+                result = False
                 if node.value == '->'  :
-                    return isArrow(grammar)
+                    result = grammar_list[index][0] == '->'
+                    
                 elif node.value == 'IDnter' :
-                    return isIDnter(grammar)
+                    result = grammar_list[index][1] == AtomType.NonTerminal
+                    
                 elif node.value == 'Elter' :
-                    return isElter(grammar)
-                elif node.value == grammar[index] :
+                    result = grammar_list[index][1] == AtomType.Terminal
+                elif node.value == grammar_list[index][0] and grammar_list[index][1] == None :
+                    result = True
+                if result :
                     index += 1
-                    return True
-                else :
-                    return False
+                return result
                 
             else : #NonTerminal
                 match node.value :
@@ -142,9 +130,4 @@ def analyse_aux(node : Node, grammar : str)-> bool :
                         next_node = A[4]
                     case other :
                         return False
-                return analyse_aux(next_node, grammar)
-    
-                    
-                
-            
-        
+                return analyse_aux(next_node, grammar_list)
--- main.py
+++ main.py
@@ -1,22 +1,29 @@
-from tree_gen import Node, AtomType, gen_conc, gen_star, gen_atom, gen_union
+from tree_gen import Node, AtomType, gen_conc, gen_star, gen_atom, gen_union, Classe
 from graphic_modules import tree_print, draw_tree, create_tree_dsplot
 from constantes import A1, A2, A3, A4, A5, A
-from analyse_modules import analyse
+from analyse_modules import analyse, scan
 
 
 if __name__ == '__main__':
     grammars = [
+        "S->[N.'->'.E.','].';',N->'IDnter',E->T.['+'.T],T->F.['.'.F],F->['IDnter'.'Elter'.('('.E.')')+('['.E.']').('(/'.E.'/)')],;",
         "S->[N.'->'.E.','].';',;",
         "N->'IDnter',;",
         "E->T.['+'.T],;",
         "T->F.['.'.F],;",
-        "F->'IDnter'+'Elter'+'('.E.')'+'['.E.']'+'(/'.E.'/)',;"
+        "F->['IDnter'.'Elter'.('('.E.')')+('['.E.']').('(/'.E.'/)')],;",
         ]
-    # for grammar in grammars:
-    #     if analyse(A[0], grammar) :
-    #         print(f'La grammaire : "{grammar}" est correcte', end = '\n\n')
-    #     else:
-    #         print(f'La grammaire "{grammar}" n\'est pas correcte', end = '\n\n')
-    for a in A:
-        tree_print(a)
-        print('\n')
+
+    list_grammar = []
+    for grammar in grammars:
+        list_grammar = scan(grammar)
+        if analyse(A[0], list_grammar) :
+            print(f'La grammaire : "{grammar}" est correcte', end = '\n\n')
+        else:
+            print(f'La grammaire "{grammar}" n\'est pas correcte', end = '\n\n')
+        
+
+    
+    
+    
+    
